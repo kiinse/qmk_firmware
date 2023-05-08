96eba640a94dfecaea1dea1e3893e7f894e3e5c5
kiinse <me@kiinse.me>
2023-05-08 20:01:11 +0300

    Update rules.mk

--- keyboards/redragon/k530/rules.mk
+++ keyboards/redragon/k530/rules.mk
@@ -48,13 +48,12 @@ MOUSEKEY_ENABLE = no    # Mouse keys
 EXTRAKEY_ENABLE = yes   # Audio control and System control
 CONSOLE_ENABLE = no     # Console for debug
 COMMAND_ENABLE = no     # Commands for debug and configuration
-SLEEP_LED_ENABLE = yes   # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes        # USB Nkey Rollover
 AUDIO_ENABLE = no
 RGBLIGHT_ENABLE = no
 SERIAL_LINK_ENABLE = no
-WAIT_FOR_USB = false
-NO_USB_STARTUP_CHECK = false
+WAIT_FOR_USB = yes
 CUSTOM_MATRIX = yes
 KEYBOARD_SHARED_EP = yes
 
