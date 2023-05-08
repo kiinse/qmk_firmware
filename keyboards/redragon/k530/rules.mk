ba8b1d87fb7d335f4a28ca1aa3c92bcb75dc8a02
kiinse <me@kiinse.me>
2023-05-08 21:14:05 +0300

    Update rules.mk

--- keyboards/redragon/k530/rules.mk
+++ keyboards/redragon/k530/rules.mk
@@ -51,6 +51,7 @@ COMMAND_ENABLE = no     # Commands for debug and configuration
 SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes        # USB Nkey Rollover
 RGBLIGHT_SLEEP = yes
+RGB_DISABLE_WHEN_USB_SUSPENDED = yes
 AUDIO_ENABLE = no
 RGBLIGHT_ENABLE = no
 SERIAL_LINK_ENABLE = no
